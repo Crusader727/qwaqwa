04d86743569d8fc8fe4b6029fa81e68e67e9f9ae
112Nick <mrnickrus@gmail.com>
2018-05-19 21:15:07 +0300

    forgot run_tests

--- run_tests.py
+++ run_tests.py
@@ -4,8 +4,9 @@ import sys
 import unittest
 from tests.tests import Tests
 from tests.suites.tests_send_document import TestsSendDocuments
+from tests.suites.test_send_stickers import TestsSendStickers
+from tests.suites.two_accaunts_management import TwoAccauntsManagement
 from tests.suites.tests_send_messages import TestsSendMessages
-
 
 if __name__ == '__main__':
     suite = unittest.TestSuite((
@@ -14,13 +15,22 @@ if __name__ == '__main__':
     suite1 = unittest.TestSuite((
         unittest.makeSuite(TestsSendDocuments),
     ))
+
     suite2 = unittest.TestSuite((
+        unittest.makeSuite(TestsSendStickers),
+    ))
+
+    suite3 = unittest.TestSuite((
+        unittest.makeSuite(TwoAccauntsManagement),
+    ))
+
+    suite4 = unittest.TestSuite((
         unittest.makeSuite(TestsSendMessages),
     ))
-    result = unittest.TextTestRunner().run(suite)
-    result1 = unittest.TextTestRunner().run(suite1)
-    result2 = unittest.TextTestRunner().run(suite2)
-    
-    sys.exit(not result.wasSuccessful() or not result1.wasSuccessful() or not result2.wasSuccessful())
-    
+    # result = unittest.TextTestRunner().run(suite)
+    # result1 = unittest.TextTestRunner().run(suite1)
+    # result2 = unittest.TextTestRunner().run(suite2)
+    # result3 = unittest.TextTestRunner().run(suite3)
+    result4 = unittest.TextTestRunner().run(suite4)
+    sys.exit(not result4.wasSuccessful())
 
